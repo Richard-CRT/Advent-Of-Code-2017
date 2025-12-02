066bbc4bab9baa607269baf13aee129bd6356833
Richard-CRT <richard-crt@users.noreply.github.com>
2025-12-02 22:56:55 +0000

    Day 4 complete

---
+++ Day_04_High-Entropy_Passphrases/Program.cs
@@ -0,0 +1,40 @@
+﻿using AdventOfCodeUtilities;
+using System.Text.RegularExpressions;
+
+List<string> inputList = AoC.GetInputLines();
+
+void P1()
+{
+    int result = 0;
+
+    foreach (string phrase in inputList)
+    {
+        string[] words = phrase.Split(' ');
+        var wordsHash = new HashSet<string>(words);
+        if (wordsHash.Count == words.Length)
+            result++;
+    }
+
+    Console.WriteLine(result);
+    Console.ReadLine();
+}
+
+void P2()
+{
+    int result = 0;
+
+    foreach (string phrase in inputList)
+    {
+        string[] words = phrase.Split(' ');
+        string[] wordsOrdered = words.Select(word => new string(word.Order().ToArray())).ToArray();
+        var wordsHash = new HashSet<string>(wordsOrdered);
+        if (wordsHash.Count == wordsOrdered.Length)
+            result++;
+    }
+
+    Console.WriteLine(result);
+    Console.ReadLine();
+}
+
+P1();
+P2();
